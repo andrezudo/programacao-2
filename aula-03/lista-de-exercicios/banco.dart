7945d48d6c07aef658e515a673df9c916cfa576f
André Luis <aluis7649@gmail.com>
2022-11-03 01:55:42 -0300

    Resposta da questão sobre banco

---
+++ aula-03/lista-de-exercicios/banco.dart
@@ -0,0 +1,85 @@
+class Cliente {
+  late String nome;
+  late String cpf;
+  Conta conta = new Conta();
+
+  void insereDados(String nome, String cpf){
+     this.nome=nome;
+     this.cpf=cpf;
+  }
+
+  void criaConta(Conta conta){
+    this.conta=conta;
+  }
+
+  @override String toString(){
+    return 'Nome: $nome - CPF: $cpf - conta $conta';
+  }
+
+  void listarConta(){
+    print('$conta');
+  }
+}
+
+class Conta {
+  late String numero;
+  late double saldo;
+
+  void criaNumero(String numero){
+    this.numero=numero;
+    this.saldo=0;
+  }
+
+  void sacar(double valor){
+    this.saldo=saldo-valor;
+  }
+
+  void depositar(double valor){
+    this.saldo=saldo+valor;
+  }
+
+  @override String toString(){
+    return 'Número: $numero - Saldo: $saldo';
+  }
+}
+
+class Banco {
+  late String nome;
+  List<Cliente> clientes = [];
+
+  void insereNome(String nome){
+    this.nome=nome;
+  }
+
+  void adicionarCliente(Cliente cliente){
+    clientes.add(cliente);
+  }
+
+  void listarClientes(){
+    int qtd=clientes.length;
+    print('Clientes');
+    for (var i = 0; i < qtd; i++) {
+      print(clientes[i]);
+    }
+  }
+}
+
+void main(List<String> args) {
+  String nome1 = 'André';
+  String cpf1 = '03042678951';
+
+  Cliente cliente = new Cliente();
+  cliente.insereDados(nome1, cpf1);
+
+  Conta conta = new Conta();
+  conta.criaNumero('01402397');
+
+  cliente.criaConta(conta);
+  cliente.conta.depositar(200);
+  cliente.conta.sacar(20);
+
+  Banco banco1 = new Banco();
+  banco1.insereNome('NuBank');
+  banco1.adicionarCliente(cliente);
+  banco1.listarClientes();
+}
