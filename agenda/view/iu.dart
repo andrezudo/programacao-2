ba63f5fb8a9ec968bbde025616465e0d7334c8ab
AndreLuis6 <handreluis.369@gmail.com>
2022-11-23 23:02:39 -0300

    Criação dos metodos

--- agenda/view/iu.dart
+++ agenda/view/iu.dart
@@ -1,3 +1,10 @@
 class IU {
-  
+  void iniciar(){
+    print('Olá! seja bem vindo a agenda!');
+    print('Escolha o que você deseja fazer');
+  }
+  void adicionar(){}
+  void listarTodos(){}
+  void remover(){}
+  void alterar(){}
 }
