8eb22a451e3d84cff0a2dedc6e9350741e1db29d
AndreLuis6 <handreluis.369@gmail.com>
2022-11-23 23:12:06 -0300

    Implementação da classe DAO

--- agenda/controller/pessoa_dao.dart
+++ agenda/controller/pessoa_dao.dart
@@ -1,5 +1,26 @@
 import '../model/bd/bd.dart';
+import 'dao.dart';
 
-class PessoaDAO {
+class PessoaDAO implements DAO{
   late BD bd;
+  
+  @override
+  void adicionar() {
+    // TODO: implement adicionar
+  }
+  
+  @override
+  void alterar() {
+    // TODO: implement alterar
+  }
+  
+  @override
+  void buscar() {
+    // TODO: implement buscar
+  }
+  
+  @override
+  void remover() {
+    // TODO: implement remover
+  }
 }
