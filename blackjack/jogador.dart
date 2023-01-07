e19a10ae6381e974ff1b9ae505ac43b66a6c62ef
AndreLuis6 <handreluis.369@gmail.com>
2023-01-06 22:33:02 -0300

    Classe jogador

---
+++ blackjack/jogador.dart
@@ -0,0 +1,16 @@
+import 'carta.dart';
+
+class Jogador {
+  String nome;
+  List<Carta> suasCartas = [];
+  int somaDasCartas = 0;
+
+  Jogador(
+    this.nome
+  );
+
+  @override 
+  String toString(){
+    return 'Nome: $nome - Soma: $somaDasCartas\n Cartas: $suasCartas';
+  }
+}
